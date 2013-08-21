b6a0915dc5349b0cc770dba0f305f7fdf411fb94
Scott Kirkland <srkirkland@gmail.com>
2013-08-20 22:23:20 -0700

    uninstalled openAuth for now, build success

--- Badges/Controllers/ApplicationController.cs
+++ Badges/Controllers/ApplicationController.cs
@@ -1,10 +1,7 @@
 using UCDArch.Web.Controller;
-using UCDArch.Web.Attributes;
 
 namespace Badges.Controllers
 {
-    [Version(MajorVersion = 3)]
-    //[ServiceMessage("Badges", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
     public abstract class ApplicationController : SuperController
     {
     }
