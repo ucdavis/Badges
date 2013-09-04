642c86280a78116ad6ca8d54402b1b60cd8ec02b
Scott Kirkland <srkirkland@gmail.com>
2013-09-04 10:17:19 -0700

    pre-fetch typeahead for titles/orgs

---
+++ Badges/Controllers/JsonController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using Badges.Core.Repositories;
+using UCDArch.Web.ActionResults;
+
+namespace Badges.Controllers
+{
+    /// <summary>
+    /// Controller for the Json class
+    /// </summary>
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class JsonController : ApplicationController
+    {
+        public JsonController(IRepositoryFactory repositoryFactory) : base(repositoryFactory)
+        {
+        }
+
+        public ActionResult Titles()
+        {
+            return new JsonNetResult(RepositoryFactory.TitleRepository.Queryable.Select(x => x.Name).ToArray());
+        }
+
+        public ActionResult Organizations()
+        {
+            return new JsonNetResult(RepositoryFactory.OrganizationRepository.Queryable.Select(x => x.Name).ToArray());
+        }
+    }
+}
