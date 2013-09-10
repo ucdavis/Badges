980be2c17fd2aba0ea27dfac936b18ce10358c26
Scott Kirkland <srkirkland@gmail.com>
2013-09-10 14:38:26 -0700

    setup basic badge controller
    also don't hide errors during dev

---
+++ Badges/Controllers/BadgeController.cs
@@ -0,0 +1,22 @@
+﻿using Badges.Core.Domain;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using Badges.Core.Repositories;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Badges.Controllers
+{
+    /// <summary>
+    /// Controller for the Badge class
+    /// </summary>
+    [Authorize(Roles=RoleNames.Student)]
+    public class BadgeController : ApplicationController
+    {
+        public BadgeController(IRepositoryFactory repositoryFactory) : base(repositoryFactory)
+        {
+
+        }
+    }
+}
