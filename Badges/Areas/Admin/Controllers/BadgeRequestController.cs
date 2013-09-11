2408903faa6f1ca646d559056c9da22863fdfc2e
Scott Kirkland <srkirkland@gmail.com>
2013-09-11 15:16:35 -0700

    admins can see pending badge requests

---
+++ Badges/Areas/Admin/Controllers/BadgeRequestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Badges.Controllers;
+using Badges.Core.Domain;
+using Badges.Core.Repositories;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Badges.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// Controller for the BadgeRequest class
+    /// </summary>
+    [Authorize(Roles = RoleNames.Administrator)]
+    public class BadgeRequestController : ApplicationController
+    {
+        public BadgeRequestController(IRepositoryFactory repositoryFactory) : base(repositoryFactory)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            var pendingRequests =
+                RepositoryFactory.BadgeRepository.Queryable.Where(x => x.Approved == false).Fetch(x => x.Creator);
+
+            return View(pendingRequests);
+        }
+    }
+}
