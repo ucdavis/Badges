7c2b7b45a37de8ec7356274e1686e4a0274cbd05
Scott Kirkland <srkirkland@gmail.com>
2013-09-13 14:10:54 -0700

    json results for experiences and work typeaheads

--- Badges/Controllers/JsonController.cs
+++ Badges/Controllers/JsonController.cs
@@ -25,5 +25,25 @@ namespace Badges.Controllers
         {
             return new JsonNetResult(RepositoryFactory.OrganizationRepository.Queryable.Select(x => x.Name).ToArray());
         }
+
+        public ActionResult MyExperiences()
+        {
+            var experiences =
+                RepositoryFactory.ExperienceRepository.Queryable.Where(
+                    x => x.Creator.Identifier == CurrentUser.Identity.Name)
+                                 .Select(x => x.Name).ToArray();
+
+            return new JsonNetResult(experiences);
+        }
+
+        public ActionResult MyWork()
+        {
+            var work =
+                RepositoryFactory.SupportingWorkRepository.Queryable.Where(
+                    x => x.Experience.Creator.Identifier == CurrentUser.Identity.Name)
+                                 .Select(x => x.Description).ToArray();
+
+            return new JsonNetResult(work);
+        }
     }
 }
