3dce4e11e3d6f510ec6313c14bb39e3a96cd5d3d
Scott Kirkland <srkirkland@gmail.com>
2013-09-19 10:19:29 -0700

    user profile shows up w/ proper info

--- Badges/Controllers/ApplicationController.cs
+++ Badges/Controllers/ApplicationController.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using Badges.Core.Domain;
 using Badges.Core.Repositories;
 using UCDArch.Web.Controller;
 
@@ -6,10 +8,24 @@ namespace Badges.Controllers
     public abstract class ApplicationController : SuperController
     {
         protected readonly IRepositoryFactory RepositoryFactory;
+        protected User AuthenticatedUser;
 
         protected ApplicationController(IRepositoryFactory repositoryFactory)
         {
             RepositoryFactory = repositoryFactory;
+        }
+
+        protected override void OnActionExecuting(System.Web.Mvc.ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                AuthenticatedUser = RepositoryFactory.UserRepository.Queryable.SingleOrDefault(
+                    user => user.Identifier == filterContext.HttpContext.User.Identity.Name);
+
+                ViewBag.AuthenticatedUser = AuthenticatedUser;
+            }
+            
+            base.OnActionExecuting(filterContext);
         }
     }
 }
