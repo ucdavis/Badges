97863cba3731e5eeebbe7ecdb650228dc52968be
Scott Kirkland <srkirkland@gmail.com>
2013-09-19 10:35:30 -0700

    fix minor introduced issue w/ admin landing

--- Badges/Areas/Admin/Controllers/LandingController.cs
+++ Badges/Areas/Admin/Controllers/LandingController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Web.Mvc;
 using Badges.Core.Domain;
+using Badges.Controllers;
+using Badges.Core.Repositories;
 
 namespace Badges.Areas.Admin.Controllers
 {
     [Authorize(Roles=RoleNames.Administrator)]
-    public class LandingController : Controller
+    public class LandingController : ApplicationController
     {
         //Admin/Landing
+        public LandingController(IRepositoryFactory repositoryFactory) : base(repositoryFactory)
+        {
+        }
+
         public ActionResult Index()
         {
             return View();
