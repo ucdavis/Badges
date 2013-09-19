f0188778025ef7ffe66114800f72f590cac47cce
Scott Kirkland <srkirkland@gmail.com>
2013-09-19 10:49:22 -0700

    image optimization

---
+++ Badges/Helpers/ImageHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SoundInTheory.DynamicImage.Fluent;
+
+namespace Badges.Helpers
+{
+    public static class ImageHelper
+    {
+        public static string GetImage(string url, int width, int height)
+        {
+            var composition =
+                new CompositionBuilder().WithLayer(
+                    LayerBuilder.Image.SourceUrl(url).WithFilter(FilterBuilder.Resize.To(width, height)));
+
+            return composition.Url;
+        }
+    }
+}
