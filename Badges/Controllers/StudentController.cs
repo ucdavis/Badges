905f52487adb81d7f5f495d5290b11b1ae151394
Hilal Alsibai <xiphirx@gmail.com>
2014-04-24 11:52:23 -0700

    Added deleteSupportingWork function for student controller

--- Badges/Controllers/StudentController.cs
+++ Badges/Controllers/StudentController.cs
@@ -126,7 +126,28 @@ namespace Badges.Controllers
             }
             RepositoryFactory.ExperienceRepository.EnsurePersistent(experience);
             RepositoryFactory.ExperienceRepository.Remove(experience);
-            return RedirectToAction("Index");
+            return RedirectToAction("Portfolio", "Student");
+        }
+
+        public ActionResult DeleteSupportingWork(Guid experienceId, Guid fileId)
+        {
+            var file = RepositoryFactory.SupportingWorkRepository.GetNullableById(fileId);
+            var experience = RepositoryFactory.ExperienceRepository.Queryable.SingleOrDefault(
+                    x => x.Id == experienceId && x.Creator.Identifier == CurrentUser.Identity.Name);
+
+            if (file == null || experience == null)
+            {
+                return new HttpNotFoundResult("Could not find the requested supporting work file or experience.");
+            }
+
+            experience.SupportingWorks.Remove(file);
+            experience.SetModified();
+            RepositoryFactory.ExperienceRepository.EnsurePersistent(experience);
+
+            RepositoryFactory.SupportingWorkRepository.Remove(file);
+            RepositoryFactory.SupportingWorkRepository.EnsurePersistent(file);
+
+            return RedirectToAction("ViewExperience", "Student", new { experienceId });
         }
 
         public ActionResult EditExperience(Guid id)
