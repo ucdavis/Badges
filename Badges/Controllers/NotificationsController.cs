2650e258c8b24cd38801dafa6ad029075e3fe63e
Hilal Alsibai <xiphirx@gmail.com>
2014-05-03 16:40:54 -0700

    add notifications controller and index view, add notification button to nav bar

---
+++ Badges/Controllers/NotificationsController.cs
@@ -0,0 +1,27 @@
+﻿using Badges.Core.Repositories;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+
+namespace Badges.Controllers
+{
+    public class NotificationsController : ApplicationController
+    {
+        public NotificationsController(IRepositoryFactory repositoryFactory) : base(repositoryFactory)
+        {
+        }
+    
+        // Auth: Administrators, Instructors, Students
+        // Displays a list of notifications for the user
+        // GET: /Notifications/
+        public ActionResult Index()
+        {
+            var notificationsList = RepositoryFactory.NotificationRepository.Queryable.Where(x => x.To.Identifier == CurrentUser.Identity.Name);
+
+            return View(notificationsList.ToList());
+        }
+
+    }
+}
