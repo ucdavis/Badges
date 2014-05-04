5da7b766d25cb8a3cc7fb4f3168dfde2a67d0808
Hilal Alsibai <xiphirx@gmail.com>
2014-05-04 00:02:11 -0700

    reverting

--- Badges.Core/Domain/Notification.cs
+++ Badges.Core/Domain/Notification.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using FluentNHibernate.Mapping;
-using System.Collections.Generic;
 
 namespace Badges.Core.Domain
 {
@@ -12,19 +11,6 @@ namespace Badges.Core.Domain
         public virtual DateTime Created { get; set; }
 
         public virtual string Message { get; set; }
-        public virtual string Subject { get; set; }
-
-        public virtual Purpose Type { get; set; }
-
-        public virtual Dictionary<string, string> Metadata { get; set; }
-
-        public enum Purpose
-        {
-            DEFAULT,
-            INSTRUCTOR_PERMISSION_REQUEST,
-            BADGE_APPROVAL,
-            BADGE_AWARD
-        }
     }
 
     public class NotificationMap : ClassMap<Notification>
@@ -36,9 +22,6 @@ namespace Badges.Core.Domain
             Map(x => x.Pending).Not.Nullable();
             Map(x => x.Created).Not.Nullable();
             Map(x => x.Message).StringMaxLength();
-            Map(x => x.Subject).StringMaxLength();
-            Map(x => x.Type).Not.Nullable();
-            Map(x => x.Metadata).Nullable();
 
             References(x => x.To).Not.Nullable();
         }
